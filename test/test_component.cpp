805ad7d30077d3c007a726f93b71f5190bafffee
Frederik Engels <frederik.engels24@gmail.com>
2018-11-26 14:51:12 +0100

    fix up gitignore

--- test/test_component.cpp
+++ test/test_component.cpp
@@ -14,13 +14,6 @@ struct random_component
 
     random_component(int i) : i{i}
     {}
-
-    friend void swap(random_component& lhs, random_component& rhs) noexcept
-    {
-        using std::swap;
-
-        swap(lhs.i, rhs.i);
-    }
 };
 
 TEST_CASE("sparse_vector")
