35dbb0e45a9dc3920e24f3b9721736406a7aac26
Frederik Engels <frederik.engels92@gmail.com>
2019-01-15 20:11:51 +0100

    sparse vector storage as default storage type

--- include/matter/component/component_traits.hpp
+++ include/matter/component/component_traits.hpp
@@ -5,6 +5,8 @@
 
 #include <type_traits>
 
+#include "matter/component/sparse_vector_storage.hpp"
+
 namespace matter
 {
 /**
@@ -20,11 +22,23 @@ template<typename C, typename = void>
 struct has_storage_type : std::false_type
 {};
 
+/// determines whether the storage_type traits was implemented for this
+/// particular component
 template<typename C>
 struct has_storage_type<C,
                         std::void_t<typename component_traits<C>::storage_type>>
     : std::true_type
 {};
+
+/// gets the storage_type set in component_traits or defaults to
+/// sparse_vector_storage<C>
+template<typename C>
+struct storage_type
+{
+    using type = std::conditional_t<has_storage_type<C>::value,
+                                    component_traits<C>::storage_type,
+                                    matter::sparse_vector_storage<C>>;
+};
 } // namespace matter
 
 #endif
