f256f665ef56baaaac1ec39e5501c61b8a04f937
Frederik Engels <frederik.engels92@gmail.com>
2019-01-20 00:39:26 +0100

    start implementation of ecs specific variant

---
+++ include/matter/util/variant.hpp
@@ -0,0 +1,20 @@
+#ifndef MATTER_UTIL_VARIANT_HPP
+#define MATTER_UTIL_VARIANT_HPP
+
+#include <type_traits>
+
+#pragma once
+
+namespace matter
+{
+namespace detail
+{
+  
+  
+}
+template<std::size_t BufSize, typename... Ts>
+struct variant
+{};
+} // namespace matter
+
+#endif
