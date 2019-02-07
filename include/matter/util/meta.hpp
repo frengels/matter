55140d7bfe86f58d6e5925ad6cc39303748cfe87
Frederik Engels <frederik.engels92@gmail.com>
2019-02-07 17:06:54 +0100

    utility for detecting specializations of templates

--- include/matter/util/meta.hpp
+++ include/matter/util/meta.hpp
@@ -9,7 +9,15 @@ namespace detail
 {
 template<typename T1, typename T2, typename U = void>
 using enable_if_same_t = std::enable_if_t<std::is_same_v<T1, T2>, U>;
-}
+
+template<typename, template<typename...> typename>
+struct is_specialization_of : std::false_type
+{};
+
+template<template<typename...> typename TTemplate, typename... Ts>
+struct is_specialization_of<TTemplate<Ts...>, TTemplate> : std::true_type
+{};
+} // namespace detail
 } // namespace matter
 
 #endif
