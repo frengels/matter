b42045e888f30123490309bddb4cf42dab9c7a83
Frederik Engels <frederik.engels92@gmail.com>
2019-02-25 17:10:25 +0100

    component_identifier as an abstraction over generated ids

--- test/test_util.cpp
+++ test/test_util.cpp
@@ -26,10 +26,27 @@ TEST_CASE("util")
 
         static_assert(matter::detail::tuple_in_list_v<std::tuple<>>);
 
+        static_assert(std::is_same_v<
+                      matter::detail::merge_non_void_t<int, float, void, int>,
+                      std::tuple<int, float, int>>);
+    }
+
+    SECTION("find types")
+    {
+        static_assert(matter::detail::search_type<float, float>::value == 0);
         static_assert(
-            std::is_same_v<
-                matter::detail::
-                    merge_non_void_t<int, float, void, int>,
-                std::tuple<int, float, int>>);
+            matter::detail::search_type<float, int, long, float>::value == 2);
+        static_assert(
+            matter::detail::search_type<float, int, float, float>::value == 1);
+
+        static_assert(
+            !matter::detail::
+                 type_index<float, int, short, char, std::tuple<float>>()
+                     .has_value());
+
+        static_assert(
+            matter::detail::
+                type_index<float, int, short, char, float, std::tuple<float>>()
+                    .value() == 3);
     }
 }
