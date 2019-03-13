34095b01827218cf3dad455704a425a0fd9dac47
Frederik Engels <frederik.engels92@gmail.com>
2019-03-13 14:01:13 +0100

    create some benchmarks to test initial things

---
+++ test/bench/insert.cpp
@@ -0,0 +1,101 @@
+#include <benchmark/benchmark.h>
+
+#include "matter/component/registry.hpp"
+
+void insert_single_vector(benchmark::State& state)
+{
+  std::vector<int> vec;
+
+  while(state.KeepRunning())
+    {
+      vec.emplace_back(42);
+    }
+}
+
+BENCHMARK(insert_single_vector)->Range(1, 1);
+
+void insert_single_first_idx(benchmark::State& state)
+{
+    matter::registry<int, char, float, uint64_t> reg;
+
+    while (state.KeepRunning())
+    {
+        reg.create<int>(std::forward_as_tuple(42));
+    }
+}
+
+BENCHMARK(insert_single_first_idx)->Range(1, 1);
+
+void insert_single_dynamic(benchmark::State& state)
+{
+    matter::registry<std::vector<int>, float, char, uint16_t> reg;
+    reg.register_component<int>();
+
+    while (state.KeepRunning())
+    {
+        reg.create<int>(std::forward_as_tuple(42));
+    }
+}
+
+BENCHMARK(insert_single_dynamic)->Range(1, 1);
+
+void insert_multiple_static(benchmark::State& state)
+{
+    matter::registry<int, float, char> reg;
+
+    while (state.KeepRunning())
+    {
+        reg.create<int, float, char>(std::forward_as_tuple(42),
+                                     std::forward_as_tuple(69.0f),
+                                     std::forward_as_tuple('!'));
+    }
+}
+
+BENCHMARK(insert_multiple_static)->Range(1, 1);
+
+void insert_multiple_static_out_of_order(benchmark::State& state)
+{
+    matter::registry<int, float, char> reg;
+
+    while (state.KeepRunning())
+    {
+        reg.create<char, int, float>(std::forward_as_tuple('!'),
+                                     std::forward_as_tuple(42),
+                                     std::forward_as_tuple(69.0f));
+    }
+}
+
+BENCHMARK(insert_multiple_static_out_of_order)->Range(1, 1);
+
+void insert_multiple_mixed(benchmark::State& state)
+{
+    matter::registry<int, float, char> reg;
+    reg.register_component<uint16_t>();
+    reg.register_component<uint8_t>();
+
+    while (state.KeepRunning())
+    {
+        reg.create<int, uint16_t, uint8_t>(std::forward_as_tuple(42),
+                                           std::forward_as_tuple(0),
+                                           std::forward_as_tuple(255));
+    }
+}
+
+BENCHMARK(insert_multiple_mixed)->Range(1, 1);
+
+void insert_multiple_runtime(benchmark::State& state)
+{
+    matter::registry<float, char> reg;
+    reg.register_component<int>();
+    reg.register_component<uint16_t>();
+    reg.register_component<uint8_t>();
+
+    while (state.KeepRunning())
+    {
+        reg.create<int, uint16_t, uint8_t>(std::forward_as_tuple(42),
+                                           std::forward_as_tuple(0),
+                                           std::forward_as_tuple(255));
+    }
+}
+
+BENCHMARK(insert_multiple_runtime)->Range(1, 1);
