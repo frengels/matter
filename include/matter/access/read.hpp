9c38ad4be1a8fee8cb980bb96c9a80746c387447
Frederik Engels <frederik.engels92@gmail.com>
2019-04-20 14:56:45 +0200

    read access specifier type and meta

---
+++ include/matter/access/read.hpp
@@ -0,0 +1,71 @@
+#ifndef MATTER_ACCESS_READ_HPP
+#define MATTER_ACCESS_READ_HPP
+
+#pragma once
+
+#include "matter/access/entity_handle.hpp"
+#include "matter/access/type_traits.hpp"
+#include "matter/component/registry.hpp"
+
+namespace matter
+{
+template<typename Registry, typename C>
+struct read_meta;
+
+template<typename C>
+struct read
+{
+    template<typename Registry>
+    using meta_type = read_meta<Registry, C>;
+
+private:
+    const C& val_;
+
+public:
+    constexpr read(const C& val) noexcept : val_{val}
+    {}
+
+    constexpr const C& get() const noexcept
+    {
+        return val_;
+    }
+};
+
+template<typename Registry, typename C>
+struct read_meta
+{
+    using required_types = C;
+
+    using registry_type = Registry;
+
+public:
+    constexpr read_meta(registry_type&) noexcept
+    {}
+
+    constexpr void process_group_vector(matter::group_vector&) const noexcept
+    {}
+
+    constexpr void process_group(const_any_group) const noexcept
+    {}
+
+    constexpr read<C> make_access(matter::entity_handle     ent,
+                                  matter::storage_handle<C> component) noexcept
+    {
+        return read{component[ent]};
+    }
+
+    // there are no commands to produce
+    constexpr void make_commands(read<C>&& r) const noexcept
+    {
+        return;
+    }
+};
+
+static_assert(matter::is_access_v<matter::read<int>, matter::registry<>>);
+
+// assert we're using the concept correctly
+static_assert(matter::is_meta_access_v<read_meta<matter::registry<>, int>>);
+
+} // namespace matter
+
+#endif
