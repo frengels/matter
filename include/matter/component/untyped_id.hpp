cc30e9bd87fd720121c7dbcffa82ce4ea54b588a
Frederik Engels <frederik.engels92@gmail.com>
2019-05-05 18:31:52 +0200

    Add more functionality to using untyped_ids
    Construction from ordered_typed_ids amongs others

--- include/matter/component/untyped_id.hpp
+++ include/matter/component/untyped_id.hpp
@@ -7,6 +7,7 @@
 #include <array>
 #include <vector>
 
+#include "matter/component/typed_id.hpp"
 #include "matter/util/algorithm.hpp"
 
 namespace matter
@@ -21,22 +22,32 @@ struct ordered_untyped_ids
     using const_iterator = const id_type*;
 
 private:
-    id_type*  ids_;
-    size_type size_;
+    const id_type* ids_;
+    size_type      size_;
 
 public:
-    constexpr ordered_untyped_ids(id_type* ids, size_type size) noexcept
+    constexpr ordered_untyped_ids(const id_type* ids, size_type size) noexcept
         : ids_{ids}, size_{size}
     {
         assert(std::is_sorted(ids_, ids_ + size));
     }
 
-    explicit constexpr ordered_untyped_ids(std::vector<id_type>& ids) noexcept
+    explicit constexpr ordered_untyped_ids(
+        const std::vector<id_type>& ids) noexcept
+        : ordered_untyped_ids{ids.data(), ids.size()}
+    {
+        // the main constructor asserts for us
+    }
+
+    template<std::size_t N>
+    explicit constexpr ordered_untyped_ids(
+        const std::array<id_type, N>& ids) noexcept
         : ordered_untyped_ids{ids.data(), ids.size()}
     {}
 
-    template<std::size_t N>
-    explicit constexpr ordered_untyped_ids(std::array<id_type, N>& ids) noexcept
+    template<typename... Ts>
+    explicit constexpr ordered_untyped_ids(
+        const matter::ordered_typed_ids<id_type, Ts...>& ids) noexcept
         : ordered_untyped_ids{ids.data(), ids.size()}
     {}
 
@@ -50,6 +61,84 @@ public:
         return ids_ + size_;
     }
 
+    constexpr bool operator==(const ordered_untyped_ids<id_type>& other) const
+        noexcept
+    {
+        return std::equal(begin(), end(), other.begin(), other.end());
+    }
+
+    constexpr bool operator!=(const ordered_untyped_ids<id_type>& other) const
+        noexcept
+    {
+        return !(*this == other);
+    }
+
+    template<typename... Ts>
+    constexpr bool
+    operator==(const matter::ordered_typed_ids<id_type, Ts...>& ids) const
+        noexcept
+    {
+        return std::equal(begin(), end(), ids.begin(), ids.end());
+    }
+
+    template<typename... Ts>
+    constexpr bool
+    operator!=(const matter::ordered_typed_ids<id_type, Ts...>& ids) const
+        noexcept
+    {
+        return !(*this == ids);
+    }
+
+    constexpr bool operator<(const ordered_untyped_ids<id_type>& other) const
+        noexcept
+    {
+        if (size() < other.size())
+        {
+            return true;
+        }
+        else if (size() > other.size())
+        {
+            return false;
+        }
+        else
+        {
+            for (std::size_t i = 0; i < size(); ++i)
+            {
+                if (operator[](i) < other[i])
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    constexpr bool operator>(const ordered_untyped_ids<id_type>& other) const
+        noexcept
+    {
+        if (size() > other.size())
+        {
+            return true;
+        }
+        else if (size() < other.size())
+        {
+            return false;
+        }
+        else
+        {
+            for (std::size_t i = 0; i < size(); ++i)
+            {
+                if (operator[](i) > other[i])
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     constexpr size_type size() const noexcept
     {
         return size_;
@@ -58,12 +147,6 @@ public:
     constexpr const id_type* data() const noexcept
     {
         return ids_;
-    }
-
-    constexpr id_type& operator[](std::size_t idx) noexcept
-    {
-        assert(idx < size());
-        return ids_[idx];
     }
 
     constexpr const id_type& operator[](std::size_t idx) const noexcept
